474ec6198c9a1810e0968d35d19f7c397d9c29c0
imse <imserre@gmail.com>
2021-06-24 01:09:42 +0200

    primeres dades

---
+++ append-timestamp.py
@@ -0,0 +1,18 @@
+import datetime
+import sys
+import shutil
+
+if len(sys.argv) != 2:
+    print("Error, You should specify an input file")
+    exit(-1)
+
+input = sys.argv[1]
+output = "output.txt"
+
+shutil.copyfile(input, output)
+
+with open(output, "a") as outputfile:
+    cur_timestamp = str(datetime.datetime.now())
+    outputfile.write("\n"+cur_timestamp)
+
+
